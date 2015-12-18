1913ece25e49dc8d90576ba986558b8703055554
Patricio López <lopezjuripatricio@gmail.com>
2015-12-18 16:20:32 -0300

    improve routes, error handling and nesting

--- app/routes/api/index.js
+++ app/routes/api/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const throwjs = require('throw.js');
 const router = express.Router();
 
 router.use('/v1', require('./v1'));
+
+router.use((err, req, res, next) => {
+  if (process.env.NODE_ENV && process.env.NODE_ENV.toUpperCase() === 'PRODUCTION') {
+    delete err.stack;
+  }
+  res.status(err.statusCode || 500).json(err);
+});
 
 module.exports = router;
--- app/routes/api/v1/organizations.js
+++ app/routes/api/v1/organizations.js
@@ -1,16 +1,32 @@
 const express = require('express');
+const throwjs = require('throw.js');
 const router = express.Router();
 
 const Organization = require('../../../models/organization');
+const Campus = require('../../../models/campus');
 
 router.route('/')
   .get((req, res, next) => {
-    Organization.find({}).then(value => res.send(value)).catch(next);
+    Organization.find().then(value => res.send(value)).catch(next);
   });
+
+router.param('id', (req, res, next, id) => {
+  Organization.findOne({ _id: id }).then(resource => {
+    req.resource = resource;
+    resource ? next() : next(new throwjs.notFound());
+  }).catch(next);
+});
 
 router.route('/:id')
   .get((req, res, next) => {
-    Organization.findOne({ _id: req.params.id }).then(value => res.send(value)).catch(next);
+    res.send(req.resource);
+  });
+
+router.route('/:id/campuses')
+  .get((req, res, next) => {
+    Organization.populate(req.resource, { path: 'campuses' }).then(resource => {
+      res.send(resource.campuses);
+    }); // TODO: catch using promises
   });
 
 module.exports = router;
