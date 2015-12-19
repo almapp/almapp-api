29015d9397d762df623572b681207039a48a0343
Patricio López <lopezjuripatricio@gmail.com>
2015-12-19 20:26:06 -0300

    support slugs on ids

--- app/routes/api/v1/helpers/id-param.js
+++ app/routes/api/v1/helpers/id-param.js
@@ -2,7 +2,10 @@ const throwjs = require('throw.js');
 
 module.exports = function(model) {
   return function(req, res, next, id) {
-    model.findOne({ _id: id })
+    const query = { $or: [{ identifier: id }] };
+    if (id.match(/^[0-9a-fA-F]{24}$/)) query.$or.push({ _id: id });
+
+    model.findOne(query)
       .then(resource => {
         req.resource = resource;
         resource ? next() : next(new throwjs.notFound());
